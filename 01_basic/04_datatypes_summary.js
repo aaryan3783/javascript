db6d61c9a0cb98ae3f3bdcb5ca91908db1af3c46
Aaryan <124796350+aaryan3783@users.noreply.github.com>
2023-09-04 21:49:09 +0000

    summary of datatypes

---
+++ 01_basic/04_datatypes_summary.js
@@ -0,0 +1,12 @@
+//primitive
+
+// 7 types: string, number, boolean, null,undefined,symbol, bigInt
+
+//reference (non-primitive)
+
+//array, objects, functions
+
+const id= Symbol("123");
+const newid=Symbol("123");
+
+console.log(id===newid);//false 
