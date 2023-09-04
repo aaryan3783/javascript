4973f3f7167098cf45c6515e0347743d3cb16b5d
Aaryan <124796350+aaryan3783@users.noreply.github.com>
2023-09-04 20:55:28 +0000

    datatypes in js

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // wer are using nodejs, not browser , in browser or console we can use alert()
+
+let name="aaryan";
+let age=20;
+let isLoggedIn=false;
+console.table([name,age,isLoggedIn]);
+
+/* data types
+number ==>  2 to power 53
+bigint
+string==>""
+boolean==> true/false
+null ==> standlone value
+undefined==> it is a data type and its means is value is not defined.
+
+symbol=>unique
+
+null==> object data type and it means value is null.
+typeof :- use to check type of data
+ex:- console.log(typeof "aaryan");
+console.log(typeof 123);
+console.log(typeof name);
+*/
+
+
+console.log(typeof "aaryan");
+console.log(typeof 123);
+console.log(typeof age);
