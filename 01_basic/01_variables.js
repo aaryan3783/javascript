b7df20ef8d299583320adf202f08552077464a64
Aaryan <124796350+aaryan3783@users.noreply.github.com>
2023-09-04 20:15:55 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId =1234;
+let accountEmail="aaryan3783@gmail.com";
+var accountPassword="12q3q";
+
+accountCity="jaipur";
+let accountState;
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountState);
+
+console.log("\n\n");
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,2 @@
 console.log("aaryan");
+console.log("aaryan");
