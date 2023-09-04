42bc1330922ffa5f15e111de1ac84f9fc08e90d2
Aaryan <124796350+aaryan3783@users.noreply.github.com>
2023-09-04 21:06:35 +0000

    conversion of data types

---
+++ 01_basic/03_conversionopertaion.js
@@ -0,0 +1,62 @@
+let score=null;
+
+console.log(typeof score);
+
+let valueIntNumber=+(score);
+console.log(typeof valueIntNumber);
+console.log(valueIntNumber);
+
+let xyz=1;
+let boolchek= Boolean(xyz);
+console.log(boolchek);
+
+
+/*
+1. with string input:- 
+
+let score='33';
+
+console.log(typeof score);
+
+let valueIntNumber=+(score);
+console.log(typeof valueIntNumber);
+console.log(valueIntNumber);
+
+
+o/p:- 
+string
+number
+33
+
+2. with input
+let score='33asd';
+
+console.log(typeof score);
+
+let valueIntNumber=+(score);
+console.log(typeof valueIntNumber);
+console.log(valueIntNumber);
+
+
+o/p: -
+string
+number
+NaN
+
+
+3. 
+let score=null;
+ o/p:- 
+ object
+number
+0
+
+4.true => 1;
+false=> 0;
+
+
+
+
+
+
+*/
