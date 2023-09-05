20a78eb94ac576428ff0019f5cae68a62da7f7b1
Aaryan <124796350+aaryan3783@users.noreply.github.com>
2023-09-05 05:57:54 +0000

    string and its methods

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,18 @@
+let age=20;
+let name='aaryan';
+
+console.log(`my name is ${name}, my age is ${age} and study in NIT AP`);
+
+//ohter method to cresate string
+const gameName= new String('ryan');
+console.log(gameName[0]);
+console.log(gameName[2]);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'));
+let name1='   aaryan   ';
+// console.log(name1.trim());
+// console.log(name1.replace('y','z'));
+
