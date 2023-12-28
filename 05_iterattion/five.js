95a229ca13af76170fd09960c5f82de84d2ec07a
Aaryan <124796350+aaryan3783@users.noreply.github.com>
2023-12-28 09:55:02 +0000

    filter and reduce

---
+++ 05_iterattion/five.js
@@ -0,0 +1,55 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+// const newNums=myNumers.map((num)=>{
+//     return num + 10;
+// })
+
+//chaining 
+const newNums=myNumers
+                    .map((num)=>num*10)
+                    .map((num)=> num+1)
+                    .filter((num)=> num>=50)
+                    
+
+console.log(newNums);
+
+//reduce method
+const myNums=[1,2,3]
+
+const myTotal=myNums.reduce((acc,currval)=>{
+    console.log(`acc: ${acc} ,currval: ${currval}`);
+return acc+currval;
+},0)
+//here 0 tells that 0 is assign to acc
+//and takes current value  i.e carrval fron array 
+console.log(myTotal);
+
+
+
+//shoping cart example
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const totalPrice=shoppingCart
+.reduce((acc,item)=>{
+    return acc+ item.price;
+},0)
+
+console.log(totalPrice);
