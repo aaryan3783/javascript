5431069d5bb9de7ae9cfffc5f6c63be543fd014e
Aaryan <124796350+aaryan3783@users.noreply.github.com>
2024-09-11 00:19:59 +0530

    Update 03_truthly.js

--- 04_Control_Flow/03_truthly.js
+++ 04_Control_Flow/03_truthly.js
@@ -6,31 +6,38 @@ if (userEmail) {
     console.log("Don't have user email");
 }
 
-// falsy values
+// falsy values:- 
 
 // false, 0, -0, BigInt 0n, "", null, undefined, NaN
 
-//truthy values
-// "0", 'false', " ", [], {}, function(){}, true
+//truthy values:- 
+// "0", 'false',"false", " ", [], {}, function(){}, true
 
+//to check array empty or not:-
 // if (userEmail.length === 0) {
 //     console.log("Array is empty");
 // }
 
+
+//to check obj empty or not:-
 const emptyObj = {}
 
 if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
 }
 
+
+//some basic notes:-  
+//false==0 (true), false==''(true), 0==''(true)
+
 // Nullish Coalescing Operator (??): null undefined
 //always allow true value . false value does not allow
 //use to handle errors
 let val1;
 // val1 = 5 ?? 10
-// val1 = null ?? 10
-val1 = undefined ?? 15
-val1 = null ?? 10 ?? 20
+// val1 = null ?? 10   //o/p 10
+val1 = undefined ?? 15   // op:- 15
+val1 = null ?? 10 ?? 20   //0p:- 10
 
 console.log(val1);
 
